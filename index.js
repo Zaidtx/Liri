b43f41354a653871b1c962a5b2b5008ecfb3bdcd
Zaid Alhir <zaidalhir@yahoo.com>
2019-11-27 18:05:04 -0600

    adding omd search API & adding bandsInTwon API

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 require('dotenv').config();
-const inuirer = reuire('inquirer');
-const axios = reuire("axios");
-const spotify = require('node-spotify-api');
+const inquirer = require('inquirer');
+const axios = require("axios");
+const Spotify = require('node-spotify-api');
 const spotify = new Spotify({
     id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
-
 });
 
 const {
@@ -16,36 +15,29 @@ const {
 
 const questions = [
     {
-    type: 'list',
-    choices: [
-        'search for song',
-        'search for movie',
-        'search for an event'
-
-    ],
-    message: 'what do you do?',
-    name:'action'
-},
-  {  
+        type: 'list',
+        choices: [
+            'Search for a song',
+            'Search for a movie',
+            'Search for an event'
+        ],
+        message: 'What do you do?',
+        name: 'action'
+    },
+    {
         type: 'input',
-        message: 'What do you want to seatch for?',
+        message: 'What do you want to search for?',
         name: 'searchFor'
-
-
     }
-
 ];
 
 const areYouDone = [
     {
-        type: 'confim',
-        message: 'Search for somthing else?',
+        type: 'confirm',
+        message: 'Search for something else?',
         name: 'continue'
     }
-
-
 ];
-
 
 function action() {
     inquirer.prompt(questions).then(function (response) {
@@ -59,35 +51,27 @@ function action() {
         });
     });
 }
-
-
 function callFunctions(action, searchFor) {
-
     switch (action) {
-        case "search for a song": getSongInfo(searchFor);
-        break;
-        case "Search for a Movie": getMovieInfro(searchFor);
-        break;
-        case "Search for event": getEventInfo(searchFor);
-        break;
-        default: console.log("I don't know what are you talking about");
-
-
+        case "Search for a song": getSongInfo(searchFor);
+            break;
+        case "Search for a movie": getMovieInfo(searchFor);
+            break;
+        case "Search for an event": getEventInfo(searchFor);
+            break;
+        default: console.log("I don;t know what you are taking about");
     }
-
 }
-
 function getSongInfo(song) {
-    console.log("sptify api");
-    spotify.searhc({ type: 'track', query: song,limit:5 }, function(err,data) {
+    console.log("spotify api")
+    spotify.search({ type: 'track', query: song,limit:5 }, function (err, data) {
         if (err) {
-            return console.log('Error occurred' + err);
-
+            return console.log('Error occurred: ' + err);
         }
-        
-        //console log 
+
+        //console.log(data.tracks.items[0]);
         const songs =data.tracks.items;
-        for(var i=0; i<songs.length; i++){
+        for(let i=0; i<songs.length; i++){
             console.log("Artist: " +songs[i].artists[0].name);
             console.log("Song Name: " +songs[i].name);
             console.log("Preview Song: " +songs[i].preview_url);
@@ -96,4 +80,41 @@ function getSongInfo(song) {
         }
     });
 }
+function getMovieInfo(movie) {
+    console.log("movie info");
+    if (movie === undefined) {
+        movie = "Mr Nobody";
+    }
+    const queryURL = "http://www.omdbapi.com/?t=" + movie + "&apikey=" + process.env.OMDB_API_KEY;
+    console.log(queryURL)
+    axios.get(queryURL).then(function (responce) {
+        //console.log(responce);
+        console.log("Title: " + responce.data.Title);
+        console.log("Year: " + responce.data.Year);
+        console.log("Actors: " + responce.data.Actors);
+        console.log("Plot: " + responce.data.Plot);
+        console.log("Language: " + responce.data.Language);
+        console.log("imdbRating: " + responce.data.imdbRating);
+
+    })
+}
+function getEventInfo(event) {
+    Console.LOG("EVENT INFO");
+    const queryURl= "https:rest.bandsintown.com/artists/"+ artist +"/events?app_id=" + process.env.BANDS_IN_TOWN_API_KEY;
+    axios.get(queryURl).then(function(responce){
+        console.log(responce);
+    console.log("dateTime: " + responce.data.datetime);
+    console.log("month:" + responce.data.month);
+    console.log("year:"+ responce.date.year);
+    console.log("Day:"+ responce.date.day);
+    
+            
+    })
+}
+
+
+
+action();
+
+
 
